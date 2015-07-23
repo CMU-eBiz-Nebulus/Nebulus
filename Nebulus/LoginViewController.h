bebfd75519906ce1d428a986dcf6e44a40b4cbd4
Jike Li <ljk5980@gmail.com>
2015-07-22 21:35:33 -0400

    init login

---
+++ Nebulus/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  Nebulus
+//
+//  Created by Jike on 7/20/15.
+//  Copyright (c) 2015 CMU-eBiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
---
+++ Nebulus/LoginViewController.m
@@ -0,0 +1,56 @@
+//
+//  LoginViewController.m
+//  Nebulus
+//
+//  Created by Jike on 7/20/15.
+//  Copyright (c) 2015 CMU-eBiz. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)login {
+    NSURL *url = [NSURL URLWithString:@"http://rest-service.guides.spring.io/greeting"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [NSURLConnection sendAsynchronousRequest:request
+                                       queue:[NSOperationQueue mainQueue]
+                           completionHandler:^(NSURLResponse *response,
+                                               NSData *data, NSError *connectionError)
+     {
+         if (data.length > 0 && connectionError == nil)
+         {
+             NSDictionary *greeting = [NSJSONSerialization JSONObjectWithData:data
+                                                                      options:0
+                                                                        error:NULL];
+             [[greeting objectForKey:@"id"] stringValue];
+             [greeting objectForKey:@"content"];
+         }
+
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
