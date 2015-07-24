26701367edf9ea83593b7f63b2086d96764bf0e5
wgtmac <ustcwg@gmail.com>
2015-07-24 16:29:26 -0400

    add new constraints to login view

--- Nebulus/AppDelegate.m
+++ Nebulus/AppDelegate.m
@@ -19,11 +19,11 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-//    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
-//    LoginViewController *loginView = [storyboard instantiateViewControllerWithIdentifier:@"loginView"];
-//    UINavigationController* navigation = [[UINavigationController alloc] initWithRootViewController:loginView];
-//    
-//    self.window.rootViewController = navigation;
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    LoginViewController *loginView = [storyboard instantiateViewControllerWithIdentifier:@"loginView"];
+    UINavigationController* navigation = [[UINavigationController alloc] initWithRootViewController:loginView];
+    
+    self.window.rootViewController = navigation;
 
     
     
