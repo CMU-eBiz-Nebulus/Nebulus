f3be15b7e0bcbda615e49df31bdaddcf2caab2e6
wgtmac <ustcwg@gmail.com>
2015-07-29 21:33:45 -0400

    add search view controller

---
+++ Nebulus/SearchViewController.m
@@ -0,0 +1,51 @@
+//
+//  SearchViewController.m
+//  Nebulus
+//
+//  Created by Gang Wu on 7/29/15.
+//  Copyright (c) 2015 CMU-eBiz. All rights reserved.
+//
+
+#import "SearchViewController.h"
+
+@interface SearchViewController () <UISearchBarDelegate,UISearchDisplayDelegate,UITableViewDelegate,UITableViewDataSource>
+
+@end
+
+@implementation SearchViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+#pragma mark - Table View Data Source
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 0;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return 0;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    return nil;
+}
+
+
+@end
