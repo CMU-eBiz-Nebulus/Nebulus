f19d748f5b0b6b56f06fcf477c73ff161494a74c
Jike Li <ljk5980@gmail.com>
2015-07-30 13:32:54 -0400

    musichttpclient

---
+++ Nebulus/MusicHttpClient.h
@@ -0,0 +1,13 @@
+//
+//  MusicHttpClient.h
+//  Nebulus
+//
+//  Created by Jike on 7/30/15.
+//  Copyright (c) 2015 CMU-eBiz. All rights reserved.
+//
+
+#import "Model.h"
+
+@interface MusicHttpClient : Model
+
+@end
