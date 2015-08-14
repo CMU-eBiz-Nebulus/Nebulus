3fe2ea91a9d4d4932cb6d1280e0a3ce07fd339ee
Jiayi Zhu <quriola@gmail.com>
2015-08-13 21:50:09 -0400

    Updated Record Setting

--- Nebulus/RecordSettingViewController.h
+++ Nebulus/RecordSettingViewController.h
@@ -7,11 +7,29 @@
 //
 
 #import <UIKit/UIKit.h>
+#define SAMPLERATE_MEDIUM 22050
+#define SAMPLERATE_LOW 11025
+#define SAMPLERATE_HIGH 44100
+
+#define BITDEPTH_LOW 8
+#define BITDEPTH_MEDIUM 8
+#define BITDEPTH_HIGH 16
+
+#define BITRATE_LOW 12000
+#define BITRATE_MEDIUM 64000
+#define BITRATE_HIGH 128000
+
+#define ESTIMATEDSIZE_LOW 0.1
+#define ESTIMATEDSIZE_MEDIUM 0.5
+#define ESTIMATEDSIZE_HIGH 1.0
+
+
 
 @interface RecordSettingViewController : UIViewController
 - (IBAction)QualityChanged:(id)sender;
 @property (weak, nonatomic) IBOutlet UISegmentedControl *Quality;
 @property (weak, nonatomic) IBOutlet UITextView *Title;
 @property (weak, nonatomic) IBOutlet UITextView *Value;
+
 
 @end
