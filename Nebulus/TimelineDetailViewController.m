b1200b0cc2370f73ac5e07fae217a91d49caef0d
wgtmac <ustcwg@gmail.com>
2015-08-17 23:00:56 -0400

    fetching comment has a bug

--- Nebulus/TimelineDetailViewController.m
+++ Nebulus/TimelineDetailViewController.m
@@ -40,7 +40,7 @@
 
 -(CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section{
     if(section == 0) return 0.1;
-    else if(section == 1) return 20.0;
+    else if(section == 1) return 30.0;
     return 0.0;
 }
 
@@ -51,8 +51,9 @@
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
     if(section == 0)
         return 3;
-    else
-        return 0;
+    else if(section == 1)
+        return [self.comments count];
+    return 0;
 }
 
 -(NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section{
@@ -66,18 +67,30 @@
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     UITableViewCell *cell = nil;
     
-    if(indexPath.row == 0){
-        cell = [tableView dequeueReusableCellWithIdentifier:@"topCell"];
-        [(UIImageView *)[cell viewWithTag:101] setImage: [UserHttpClient getUserImage:self.activity.creator.objectID]];
-        [(UILabel *)[cell viewWithTag:102] setText:self.activity.creator.username];
-        [(UILabel *)[cell viewWithTag:103] setText:[self.activity.tags componentsJoinedByString:@", "]];
-        [(UILabel *)[cell viewWithTag:104] setText:self.activity.title];
-    } else if(indexPath.row == 1){
-        cell = [tableView dequeueReusableCellWithIdentifier:@"textCell"];
-        UITextView *textView = (UITextView *)[cell viewWithTag:101];
-        [textView setText:self.activity.text];
-    } else {
-        cell = [tableView dequeueReusableCellWithIdentifier:@"buttomCell"];
+    if(indexPath.section == 0){
+        if(indexPath.row == 0){
+            cell = [tableView dequeueReusableCellWithIdentifier:@"topCell"];
+            [(UIImageView *)[cell viewWithTag:101] setImage: [UserHttpClient getUserImage:self.activity.creator.objectID]];
+            [(UILabel *)[cell viewWithTag:102] setText:self.activity.creator.username];
+            [(UILabel *)[cell viewWithTag:103] setText:[self.activity.tags componentsJoinedByString:@", "]];
+            [(UILabel *)[cell viewWithTag:104] setText:self.activity.title];
+        } else if(indexPath.row == 1){
+            cell = [tableView dequeueReusableCellWithIdentifier:@"textCell"];
+            UITextView *textView = (UITextView *)[cell viewWithTag:101];
+            [textView setText:self.activity.text];
+        } else {
+            cell = [tableView dequeueReusableCellWithIdentifier:@"buttomCell"];
+        }
+    }else if(indexPath.section == 1){
+        cell = [tableView dequeueReusableCellWithIdentifier:@"commentCell"];
+        
+        Comment *comment = [self.comments objectAtIndex:indexPath.row];
+//        [(UIImageView *)[cell viewWithTag:101] setImage: [UserHttpClient getUserImage:comment.sender.objectID]];
+        [(UILabel *)[cell viewWithTag:102] setText:comment.sender.username];
+//        
+ //       [(UITextView *)[cell viewWithTag:103] setText:comment.text];
+        [(UIButton *)[cell viewWithTag:104] setHidden:NO];   // LIKE button
+        [(UIButton *)[cell viewWithTag:105] setHidden:YES];  // PLAY button
     }
     
     return cell;
@@ -87,13 +100,19 @@
 #define HEIGHT_BOTTOM_CELL  35.0
 #define HEIGHT_TEXT_CELL    35.0
 #define HEIGHT_PLAY_CELL    45.0
+#define HEIGHT_COMMENT_CELL 60.0
 -(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
-    if(indexPath.row == 0){
-        return HEIGHT_TOP_CELL;
-    } else if(indexPath.row == 1){
-        return HEIGHT_TEXT_CELL;
-    } else {
-        return HEIGHT_BOTTOM_CELL;
+
+    if(indexPath.section == 0){
+        if(indexPath.row == 0){
+            return HEIGHT_TOP_CELL;
+        } else if(indexPath.row == 1){
+            return HEIGHT_TEXT_CELL;
+        } else {
+            return HEIGHT_BOTTOM_CELL;
+        }
+    }else if(indexPath.section == 1){
+        return HEIGHT_COMMENT_CELL;
     }
     return 0.0;
 }
